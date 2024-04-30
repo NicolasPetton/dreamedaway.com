2e50766b24fd489f70df4604d7632df1c46629fe
Nicolas Petton <nicolas@petton.fr>
2024-04-30 10:47:12 +0200

    Backer portal CSS tweaks and region-based linking support

--- docs/js/reward_cards.js
+++ docs/js/reward_cards.js
@@ -11,12 +11,18 @@
 
 	let setupCard = (card) => {
 		card.addEventListener("click", () => {
-			toggleModal();
+			var euLink = card .getAttribute("data-eu-link");
+			var worldLink = card.getAttribute("data-world-link");
+			showModal({euLink, worldLink});
 		});
 	};
 
-	let toggleModal = () => {
+	let showModal = ({euLink, worldLink}) => {
 		var modal = document.querySelector("#dialog");
+
+		document.querySelector("#dialog .flags [data-toggle='eu']").href = euLink;
+		document.querySelector("#dialog .flags [data-toggle='world']").href = worldLink;
+
 		modal.classList.toggle("visible");
 	};
 
@@ -27,11 +33,19 @@
 	let setupFlag = (flag) => {
 		var toggle = flag.getAttribute("data-toggle");
 		flag.addEventListener("mouseover", () => {
-			[...document.querySelectorAll("#dialog .legend span")].forEach((e) => {
+			document.querySelectorAll("#dialog .legend span").forEach((e) => {
 				e.classList.remove("visible");
 			});
 
 			document.querySelector(`#dialog .legend span.${toggle}`).classList.add("visible");
+		});
+
+		flag.addEventListener("mouseout", () => {
+			document.querySelectorAll("#dialog .legend span").forEach((e) => {
+				e.classList.remove("visible");
+			});
+
+			document.querySelector(`#dialog .legend span:first-child`).classList.add("visible");
 		});
 	};
 
