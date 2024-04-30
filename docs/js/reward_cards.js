684996fe5d415ab713688620dc91ede9bf51a38c
Nicolas Petton <nicolas@petton.fr>
2024-04-30 11:58:32 +0200

    JS formatting tweaks

--- docs/js/reward_cards.js
+++ docs/js/reward_cards.js
@@ -2,57 +2,48 @@
 	let setup = () => {
 		setupCards();
 		setupFlags();
-		setupModal();
+		setupDialog();
 	};
 
-	let setupCards = () => {
-		document.querySelectorAll(".reward.physical").forEach(setupCard);
+	let setupCards = () => document.querySelectorAll(".reward.physical").forEach(setupCard);
+
+	let setupCard = card => {
+		var euLink = card .getAttribute("data-eu-link");
+		var worldLink = card.getAttribute("data-world-link");
+
+		card.addEventListener("click", () => showDialog({euLink, worldLink}));
 	};
 
-	let setupCard = (card) => {
-		card.addEventListener("click", () => {
-			var euLink = card .getAttribute("data-eu-link");
-			var worldLink = card.getAttribute("data-world-link");
-			showModal({euLink, worldLink});
-		});
-	};
-
-	let showModal = ({euLink, worldLink}) => {
-		var modal = document.querySelector("#dialog");
+	let showDialog = ({euLink, worldLink}) => {
+		var dialog = document.querySelector("#dialog");
 
 		document.querySelector("#dialog .flags [data-toggle='eu']").href = euLink;
 		document.querySelector("#dialog .flags [data-toggle='world']").href = worldLink;
 
-		modal.classList.toggle("visible");
+		dialog.classList.toggle("visible");
 	};
 
-	let setupFlags = () => {
-		document.querySelectorAll("#dialog .flags a").forEach(setupFlag);
-	};
+	let setupFlags = () => document.querySelectorAll("#dialog .flags a").forEach(setupFlag);
 
-	let setupFlag = (flag) => {
+	let setupFlag = flag => {
 		var toggle = flag.getAttribute("data-toggle");
-		flag.addEventListener("mouseover", () => {
-			document.querySelectorAll("#dialog .legend span").forEach((e) => {
-				e.classList.remove("visible");
-			});
+		var flags = document.querySelectorAll("#dialog .legend span");
 
+		flag.addEventListener("mouseover", () => {
+			flags.forEach(e => e.classList.remove("visible"));
 			document.querySelector(`#dialog .legend span.${toggle}`).classList.add("visible");
 		});
 
 		flag.addEventListener("mouseout", () => {
-			document.querySelectorAll("#dialog .legend span").forEach((e) => {
-				e.classList.remove("visible");
-			});
-
+			flags.forEach(e => e.classList.remove("visible"));
 			document.querySelector(`#dialog .legend span:first-child`).classList.add("visible");
 		});
 	};
 
-	let setupModal = () => {
-		document.querySelector("#dialog .close").addEventListener("click", () => {
-			document.querySelector("#dialog").classList.remove("visible");
-		});
+	let setupDialog = () => {
+		document.querySelector("#dialog .close").addEventListener("click", () =>
+			document.querySelector("#dialog").classList.remove("visible")
+		);
 	};
 
 	document.addEventListener('DOMContentLoaded', setup);
